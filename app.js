19f963f8fafd4837edb065f17293e15ea95e2b2e
Adrish Dey <adrishdey2007@gmail.com>
2026-07-27 12:45:02 +0530

    foundation/CRUD API-s added

--- app.js
+++ app.js
@@ -4,9 +4,18 @@ require('dotenv').config();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const port = 8080;
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require('ejs-mate');
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.engine("ejs",ejsMate);
 
 
-async function main(){
+async function main() {
     await mongoose.connect(process.env.MONGO_URI);
 }
 main().then(() => {
@@ -19,22 +28,48 @@ main().then(() => {
     console.log("error connecting to MongoDB", err);
 })
 
-app.get("/",(req,res) => {
+app.get("/", (req, res) => {
     res.send("hi i am root");
 });
-
-app.get("/listingTest", async (req,res) => {
-    const sampleListing = new Listing({
-        title : "My new villa",
-        description : "This villa stands at the conjungtion of the spheres of sea and land",
-        image : "https://www.pinterest.com/ideas/house-in-london-aesthetic/930533389747/",
-        price: 1000000,
-        location : "Edinbourg",
-        country : "Scotland"
-    })
-    await sampleListing.save();
-    console.log("sample was saved ");
-    res.send("sample data saved successfully");
+// Index Route
+app.get("/listings", async (req, res) => {
+    const allListings = await Listing.find();
+    res.render("listings/index.ejs", { allListings });
 })
 
+// New Route
+app.get("/listings/new", (req, res) => {
+    res.render("listings/new");
+});
+
+// Show Route
+app.get("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", { listing });
+})
+// Edit Route
+app.get("/listings/:id/edit", async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", { listing });
+})
+// Update Route
+app.put("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    res.redirect(`/listings/${id}`);
+})
+// Create Route
+app.post("/listings", async (req, res) => {
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+})
+// Delete Route
+app.delete("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+})
 
